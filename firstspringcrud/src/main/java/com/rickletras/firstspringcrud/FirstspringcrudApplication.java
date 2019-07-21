c6c6906063b356bc82637051705919df9b103ced
Ricardo Lima <rickletras@gmail.com>
2019-07-21 14:32:45 -0300

    Primeiro commit - projeto criado

---
+++ firstspringcrud/src/main/java/com/rickletras/firstspringcrud/FirstspringcrudApplication.java
@@ -0,0 +1,13 @@
+package com.rickletras.firstspringcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspringcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstspringcrudApplication.class, args);
+	}
+
+}
